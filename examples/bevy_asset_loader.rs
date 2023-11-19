28dce477b64e7738c2f92400c037a70790acf359
KirmesBude <kirmesbude@gmail.com>
2023-11-19 10:44:56 +0100

    Add example with bevy_asset_loader

---
+++ examples/bevy_asset_loader.rs
@@ -0,0 +1,85 @@
+//! Adapted from https://github.com/NiklasEi/bevy_asset_loader/blob/b372b972e67a2e4b076442d23dcaff07083da611/bevy_asset_loader/examples/atlas_from_grid.rs
+//! Renders an animated sprite by loading all animation frames from a single image (a sprite sheet)
+//! into a texture atlas, and changing the displayed image periodically.
+
+use bevy::prelude::*;
+use bevy_asset_loader::prelude::*;
+
+/// This example demonstrates how to load a texture atlas from a sprite sheet
+///
+/// Requires the feature '2d'
+fn main() {
+    App::new()
+        .add_state::<MyStates>()
+        .add_plugins(DefaultPlugins)
+        .add_loading_state(
+            LoadingState::new(MyStates::AssetLoading).continue_to_state(MyStates::Next),
+        )
+        .add_collection_to_loading_state::<_, MyAssets>(MyStates::AssetLoading)
+        .add_systems(OnEnter(MyStates::Next), draw_atlas)
+        .add_systems(
+            Update,
+            animate_sprite_system.run_if(in_state(MyStates::Next)),
+        )
+        .run();
+}
+
+#[derive(AssetCollection, Resource)]
+struct MyAssets {
+    #[asset(path = "gabe-idle-run.titan")]
+    female_adventurer: Handle<TextureAtlas>,
+}
+
+fn draw_atlas(
+    mut commands: Commands,
+    my_assets: Res<MyAssets>,
+    texture_atlases: Res<Assets<TextureAtlas>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+    // draw the original image (whole atlas)
+    let atlas = texture_atlases
+        .get(&my_assets.female_adventurer)
+        .expect("Failed to find our atlas");
+    commands.spawn(SpriteBundle {
+        texture: atlas.texture.clone(),
+        transform: Transform::from_xyz(0., -150., 0.),
+        ..Default::default()
+    });
+    // draw single texture from sprite sheet starting at index 0
+    commands
+        .spawn(SpriteSheetBundle {
+            transform: Transform {
+                translation: Vec3::new(0., 150., 0.),
+                ..Default::default()
+            },
+            sprite: TextureAtlasSprite::new(0),
+            texture_atlas: my_assets.female_adventurer.clone(),
+            ..Default::default()
+        })
+        .insert(AnimationTimer(Timer::from_seconds(
+            0.1,
+            TimerMode::Repeating,
+        )));
+}
+
+#[derive(Component)]
+struct AnimationTimer(Timer);
+
+fn animate_sprite_system(
+    time: Res<Time>,
+    mut query: Query<(&mut AnimationTimer, &mut TextureAtlasSprite)>,
+) {
+    for (mut timer, mut sprite) in &mut query {
+        timer.0.tick(time.delta());
+        if timer.0.finished() {
+            sprite.index = (sprite.index + 1) % 8;
+        }
+    }
+}
+
+#[derive(Clone, Eq, PartialEq, Debug, Hash, Default, States)]
+enum MyStates {
+    #[default]
+    AssetLoading,
+    Next,
+}
