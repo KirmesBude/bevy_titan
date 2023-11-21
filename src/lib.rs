624ce1d8ee48a52374bd6b5ee5b85c0f38c6d109
KirmesBude <kirmesbude@gmail.com>
2023-11-21 17:27:56 +0100

    Implement new format fully

--- src/lib.rs
+++ src/lib.rs
@@ -123,7 +123,8 @@ impl AssetLoader for SpriteSheetLoader {
         Box::pin(async move {
             let mut bytes = Vec::new();
             reader.read_to_end(&mut bytes).await?;
-            let titan_entries = ron::de::from_bytes::<Vec<TitanEntry>>(&bytes)?;
+            let titan = ron::de::from_bytes::<Titan>(&bytes)?;
+            let titan_entries = titan.textures;
 
             /* Save rect ids and images for later use */
             let rect_ids_len = titan_entries.iter().fold(0, |acc, titan_entry| {
@@ -149,7 +150,7 @@ impl AssetLoader for SpriteSheetLoader {
                     TitanSpriteSheet::None => {
                         let rect_id = RectId {
                             image_index: titan_entry_index,
-                            position: Position::ZERO,
+                            position: TitanUVec2::ZERO,
                             size: image.size().into(),
                         };
                         rect_ids.push(rect_id);
@@ -174,7 +175,7 @@ impl AssetLoader for SpriteSheetLoader {
 
                                 let rect_id = RectId {
                                     image_index: titan_entry_index,
-                                    position: Position { x, y },
+                                    position: TitanUVec2(x, y),
                                     size: tile_size.into(),
                                 };
                                 rect_ids.push(rect_id);
@@ -201,7 +202,7 @@ impl AssetLoader for SpriteSheetLoader {
             let mut rects_to_place = GroupedRectsToPlace::<RectId>::new();
             rect_ids.iter().for_each(|rect_id| {
                 let rect_to_insert =
-                    RectToInsert::new(rect_id.size.width as u32, rect_id.size.height as u32, 1);
+                    RectToInsert::new(rect_id.size.width() as u32, rect_id.size.height() as u32, 1);
                 rects_to_place.push_rect(rect_id.clone(), None, rect_to_insert);
             });
 
@@ -280,8 +281,122 @@ impl AssetLoader for SpriteSheetLoader {
 #[derive(Debug, Deserialize)]
 struct Titan {
     #[serde(default)]
-    configuration: Option<()>,
+    configuration: TitanConfiguration,
     textures: Vec<TitanEntry>,
+}
+
+#[derive(Debug, Deserialize)]
+struct TitanConfiguration {
+    initial_size: TitanUVec2,
+    max_size: TitanUVec2,
+    format: TitanTextureFormat,
+    auto_format_conversion: bool,
+    padding: TitanUVec2, /* TODO: Another type? */
+}
+
+impl Default for TitanConfiguration {
+    fn default() -> Self {
+        Self {
+            initial_size: TitanUVec2(256, 265),
+            max_size: TitanUVec2(2048, 2048),
+            format: TitanTextureFormat(TextureFormat::Rgba8UnormSrgb),
+            auto_format_conversion: true,
+            padding: TitanUVec2::ZERO,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct TitanTextureFormat(TextureFormat);
+
+impl<'de> Deserialize<'de> for TitanTextureFormat {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?.to_lowercase();
+        let texture_format = match s.as_str() {
+            "R8Unorm" => TextureFormat::R8Unorm,
+            "R8Snorm" => TextureFormat::R8Snorm,
+            "R8Uint" => TextureFormat::R8Uint,
+            "R8Sint" => TextureFormat::R8Sint,
+            "R16Uint" => TextureFormat::R16Uint,
+            "R16Sint" => TextureFormat::R16Sint,
+            "R16Unorm" => TextureFormat::R16Unorm,
+            "R16Snorm" => TextureFormat::R16Snorm,
+            "R16Float" => TextureFormat::R16Float,
+            "Rg8Unorm" => TextureFormat::Rg8Unorm,
+            "Rg8Snorm" => TextureFormat::Rg8Snorm,
+            "Rg8Uint" => TextureFormat::Rg8Uint,
+            "Rg8Sint" => TextureFormat::Rg8Sint,
+            "R32Uint" => TextureFormat::R32Uint,
+            "R32Sint" => TextureFormat::R32Sint,
+            "R32Float" => TextureFormat::R32Float,
+            "Rg16Uint" => TextureFormat::Rg16Uint,
+            "Rg16Sint" => TextureFormat::Rg16Sint,
+            "Rg16Unorm" => TextureFormat::Rg16Unorm,
+            "Rg16Snorm" => TextureFormat::Rg16Snorm,
+            "Rg16Float" => TextureFormat::Rg16Float,
+            "Rgba8Unorm" => TextureFormat::Rgba8Unorm,
+            "Rgba8UnormSrgb" => TextureFormat::Rgba8UnormSrgb,
+            "Rgba8Snorm" => TextureFormat::Rgba8Snorm,
+            "Rgba8Uint" => TextureFormat::Rgba8Uint,
+            "Rgba8Sint" => TextureFormat::Rgba8Sint,
+            "Bgra8Unorm" => TextureFormat::Bgra8Unorm,
+            "Bgra8UnormSrgb" => TextureFormat::Bgra8UnormSrgb,
+            "Rgb9e5Ufloat" => TextureFormat::Rgb9e5Ufloat,
+            "Rgb10a2Unorm" => TextureFormat::Rgb10a2Unorm,
+            "Rg11b10Float" => TextureFormat::Rg11b10Float,
+            "Rg32Uint" => TextureFormat::Rg32Uint,
+            "Rg32Sint" => TextureFormat::Rg32Sint,
+            "Rg32Float" => TextureFormat::Rg32Float,
+            "Rgba16Uint" => TextureFormat::Rgba16Uint,
+            "Rgba16Sint" => TextureFormat::Rgba16Sint,
+            "Rgba16Unorm" => TextureFormat::Rgba16Unorm,
+            "Rgba16Snorm" => TextureFormat::Rgba16Snorm,
+            "Rgba16Float" => TextureFormat::Rgba16Float,
+            "Rgba32Uint" => TextureFormat::Rgba32Uint,
+            "Rgba32Sint" => TextureFormat::Rgba32Sint,
+            "Rgba32Float" => TextureFormat::Rgba32Float,
+            "Stencil8" => TextureFormat::Stencil8,
+            "Depth16Unorm" => TextureFormat::Depth16Unorm,
+            "Depth24Plus" => TextureFormat::Depth24Plus,
+            "Depth24PlusStencil8" => TextureFormat::Depth24PlusStencil8,
+            "Depth32Float" => TextureFormat::Depth32Float,
+            "Depth32FloatStencil8" => TextureFormat::Depth32FloatStencil8,
+            "Bc1RgbaUnorm" => TextureFormat::Bc1RgbaUnorm,
+            "Bc1RgbaUnormSrgb" => TextureFormat::Bc1RgbaUnormSrgb,
+            "Bc2RgbaUnorm" => TextureFormat::Bc2RgbaUnorm,
+            "Bc2RgbaUnormSrgb" => TextureFormat::Bc2RgbaUnormSrgb,
+            "Bc3RgbaUnorm" => TextureFormat::Bc3RgbaUnorm,
+            "Bc3RgbaUnormSrgb" => TextureFormat::Bc3RgbaUnormSrgb,
+            "Bc4RUnorm" => TextureFormat::Bc4RUnorm,
+            "Bc4RSnorm" => TextureFormat::Bc4RSnorm,
+            "Bc5RgUnorm" => TextureFormat::Bc5RgUnorm,
+            "Bc5RgSnorm" => TextureFormat::Bc5RgSnorm,
+            "Bc6hRgbUfloat" => TextureFormat::Bc6hRgbUfloat,
+            "Bc6hRgbFloat" => TextureFormat::Bc6hRgbFloat,
+            "Bc7RgbaUnorm" => TextureFormat::Bc7RgbaUnorm,
+            "Bc7RgbaUnormSrgb" => TextureFormat::Bc7RgbaUnormSrgb,
+            "Etc2Rgb8Unorm" => TextureFormat::Etc2Rgb8Unorm,
+            "Etc2Rgb8UnormSrgb" => TextureFormat::Etc2Rgb8UnormSrgb,
+            "Etc2Rgb8A1Unorm" => TextureFormat::Etc2Rgb8A1Unorm,
+            "Etc2Rgb8A1UnormSrgb" => TextureFormat::Etc2Rgb8A1UnormSrgb,
+            "Etc2Rgba8Unorm" => TextureFormat::Etc2Rgba8Unorm,
+            "Etc2Rgba8UnormSrgb" => TextureFormat::Etc2Rgba8UnormSrgb,
+            "EacR11Unorm" => TextureFormat::EacR11Unorm,
+            "EacR11Snorm" => TextureFormat::EacR11Snorm,
+            "EacRg11Unorm" => TextureFormat::EacRg11Unorm,
+            "EacRg11Snorm" => TextureFormat::EacRg11Snorm,
+            other => {
+                return Err(serde::de::Error::custom(format!(
+                    "Invalid variant '{}'",
+                    other
+                )));
+            }
+        };
+        Ok(TitanTextureFormat(texture_format))
+    }
 }
 
 #[derive(Debug, Deserialize)]
@@ -304,47 +419,48 @@ enum TitanSpriteSheet {
         #[serde(default)]
         offset: Option<UVec2>,
     },
-    Heterogeneous(Vec<(Position, Size)>),
+    Heterogeneous(Vec<(TitanUVec2, TitanUVec2)>),
 }
 
 #[derive(Debug, Hash, PartialEq, Eq, Clone, Ord, PartialOrd)]
 struct RectId {
     image_index: usize,
-    position: Position,
-    size: Size,
+    position: TitanUVec2,
+    size: TitanUVec2,
 }
 
 #[derive(Debug, Hash, PartialEq, Eq, Clone, Ord, PartialOrd, Copy, Deserialize)]
-struct Position {
-    x: usize,
-    y: usize,
-}
+struct TitanUVec2(usize, usize);
 
-impl Position {
-    const ZERO: Self = Self { x: 0, y: 0 };
-}
+impl TitanUVec2 {
+    const ZERO: Self = Self(0, 0);
 
-#[derive(Debug, Hash, PartialEq, Eq, Clone, Ord, PartialOrd, Copy, Deserialize)]
-struct Size {
-    width: usize,
-    height: usize,
-}
+    fn x(&self) -> usize {
+        self.0
+    }
 
-impl From<UVec2> for Size {
-    fn from(value: UVec2) -> Self {
-        Self {
-            width: value.x as usize,
-            height: value.y as usize,
-        }
+    fn y(&self) -> usize {
+        self.1
+    }
+
+    fn width(&self) -> usize {
+        self.0
+    }
+
+    fn height(&self) -> usize {
+        self.1
     }
 }
 
-impl From<&UVec2> for Size {
+impl From<UVec2> for TitanUVec2 {
+    fn from(value: UVec2) -> Self {
+        Self(value.x as usize, value.y as usize)
+    }
+}
+
+impl From<&UVec2> for TitanUVec2 {
     fn from(value: &UVec2) -> Self {
-        Self {
-            width: value.x as usize,
-            height: value.y as usize,
-        }
+        Self(value.x as usize, value.y as usize)
     }
 }
 
@@ -352,7 +468,7 @@ fn copy_rect_image_to_texture_atlas(
     texture_atlas: &mut Image,
     location: &PackedLocation,
     image: &Image,
-    position: Position,
+    position: TitanUVec2,
 ) {
     let format_size = texture_atlas.texture_descriptor.format.pixel_size();
     let rect_x = location.x() as usize;
@@ -365,7 +481,7 @@ fn copy_rect_image_to_texture_atlas(
     for i in 0..rect_height {
         let texture_atlas_begin = (rect_x + ((rect_y + i) * texture_atlas_width)) * format_size;
         let texture_atlas_end = texture_atlas_begin + rect_width * format_size;
-        let data_begin = (position.x + (position.y + i) * image.width() as usize) * format_size;
+        let data_begin = (position.x() + (position.y() + i) * image.width() as usize) * format_size;
         let data_end = data_begin + rect_width * format_size;
 
         texture_atlas.data[texture_atlas_begin..texture_atlas_end]
