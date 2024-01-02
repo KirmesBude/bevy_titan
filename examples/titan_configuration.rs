f9284046c76d8ac47779204970ad62abb6b759be
KirmesBude <kirmesbude@gmail.com>
2024-01-02 17:12:18 +0100

    Finish all examples

--- examples/titan_configuration.rs
+++ examples/titan_configuration.rs
@@ -10,7 +10,7 @@ fn main() {
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest())) // prevents blurry sprites
         .add_plugins(SpriteSheetLoaderPlugin)
         .add_systems(Startup, setup)
-        .add_systems(Update, animate_sprite)
+        .add_systems(Update, (animate_sprite, spawn_entire_texture_atlas))
         .run();
 }
 
@@ -47,4 +47,26 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         },
         AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
     ));
+}
+
+fn spawn_entire_texture_atlas(
+    mut commands: Commands,
+    mut asset_event_evr: EventReader<AssetEvent<TextureAtlas>>,
+    texture_atlas_assets: Res<Assets<TextureAtlas>>,
+) {
+    for ev in asset_event_evr.read() {
+        match ev {
+            AssetEvent::Added { id } => {
+                let texture_atlas = texture_atlas_assets.get(*id).unwrap();
+
+                commands.spawn(SpriteBundle {
+                    texture: texture_atlas.texture.clone(),
+                    transform: Transform::from_translation(Vec3::new(-300.0, 0.0, -1.0))
+                        .with_scale(Vec3::splat(3.0)),
+                    ..Default::default()
+                });
+            }
+            _ => {}
+        }
+    }
 }
